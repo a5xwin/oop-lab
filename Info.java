db14ece7909558a849510317039949e37f4fcd25
ash <ashmercesletifercoc@gmail.com>
2023-10-12 22:40:50 +0530

    Remove all files

--- Info.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Info{
-    public static void main(String args[]){
-        System.out.println("Name: Ashwin");
-        System.out.println("Age: 19");
-        System.out.println("Address: Vaishnavam, Mamala P.O, Thiruvankulam, Ernakulam");
-    }
-}
