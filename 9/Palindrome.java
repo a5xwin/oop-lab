d5e6652b64782522764978f57645ea64b73ca8fe
ash <ashmercesletifercoc@gmail.com>
2023-10-13 08:32:07 +0530

    code updated

---
+++ 9/Palindrome.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Palindrome{
+
+	public static void main(String args[]){
+	
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.print("Enter any string: ");
+		String x =sc.nextLine();
+
+		int len=x.length();
+
+		int i=0;
+		int j=len-1;
+		int count=0;
+
+
+		while(i<j && count==0){
+
+			if(x.charAt(i)!=(x.charAt(j))){
+				count+=1;
+			}
+			i++;
+			j--;
+		}
+
+		
+		if(count!=0){
+			System.out.println("Entered string is not palindrome!");
+		}
+		
+
+		else{
+			System.out.println("Entered string is palindrome!");
+		}
+	}		
+}
